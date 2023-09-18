91523f726f793a11d745d8ea8fcdf32be36060d6
Trong <trantrong987@gmail.com>
2023-09-18 17:21:11 +0700

    Trong commit mobile, sử dụng trong test_flutter

--- test_flutter/lib/main.dart
+++ test_flutter/lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:test_flutter/Account.dart';
+import 'package:test_flutter/Import.dart';
+import 'package:test_flutter/Itemdata.dart';
+import 'package:test_flutter/Itemmaster.dart';
+import 'package:test_flutter/Location.dart';
+import 'package:test_flutter/QC.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,115 +17,261 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Warehouses',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List dataFake = [
+    "Accounts",
+    "Import Item",
+    "Item Data",
+    "Item Master",
+    "Location",
+    "QC",
+  ];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  List<Color> dataFakeColor = [
+    Color(0xffffcf2f),
+    Color(0xff6fe08d),
+    Color(0xff61bdfd),
+    Color(0xfffc7f7f),
+    Color(0xffc884fb),
+    Color(0xff78e667),
+  ];
+
+  List<Icon> dataFakeIcon = [
+    Icon(Icons.manage_accounts, color: Colors.white, size: 30),
+    Icon(Icons.import_export, color: Colors.white, size: 30),
+    Icon(Icons.data_thresholding, color: Colors.white, size: 30),
+    Icon(Icons.mark_as_unread, color: Colors.white, size: 30),
+    Icon(Icons.location_on, color: Colors.white, size: 30),
+    Icon(Icons.check_box_rounded, color: Colors.white, size: 30),
+  ];
+
+  List<Widget> pages = [
+    const Account(),
+    const Import(),
+    const Itemdata(),
+    const Itemmaster(),
+    const Location(),
+    const QC(),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      body: ListView(
+        children: [
+          Container(
+            padding: EdgeInsets.only(top: 15, left: 15, right: 15, bottom: 10),
+            decoration: BoxDecoration(
+                color: Color(0xff674AEF),
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(20),
+                  bottomRight: Radius.circular(20),
+                )),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Builder(
+                      builder: (BuildContext context) {
+                        return InkWell(
+                          onTap: () {
+                            Scaffold.of(context).openDrawer();
+                          },
+                          child: const Icon(
+                            Icons.dashboard,
+                            size: 30,
+                            color: Colors.white,
+                          ),
+                        );
+                      },
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Padding(
+                  padding: EdgeInsets.only(left: 3, bottom: 15),
+                  child: Text(
+                    "Trong Nguyen",
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: 1,
+                      wordSpacing: 2,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 5, bottom: 20),
+                  width: MediaQuery.of(context).size.width,
+                  height: 55,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Search here...",
+                        hintStyle: TextStyle(
+                          color: Colors.black.withOpacity(0.5),
+                        ),
+                        prefixIcon: Icon(
+                          Icons.search,
+                          size: 25,
+                        ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              top: 20,
+              left: 15,
+              right: 15,
+            ),
+            child: Column(
+              children: [
+                GridView.builder(
+                  itemCount: dataFake.length,
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    childAspectRatio: 1.1,
+                  ),
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => pages[index]),
+                        );
+                      },
+                      child: Column(
+                        children: [
+                          Container(
+                            height: 60,
+                            width: 60,
+                            decoration: BoxDecoration(
+                                color: dataFakeColor[index],
+                                shape: BoxShape.circle),
+                            child: Center(
+                              child: Center(
+                                child: dataFakeIcon[index],
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            dataFake[index],
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.black.withOpacity(0.7),
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Warehouse",
+                      style: TextStyle(
+                        fontSize: 23,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      "See All",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
+                        color: Color(0xff674AEF),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          )
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      drawer: Drawer(
+        elevation: 0,
+        width: 270,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(8),
+              bottomLeft: Radius.circular(8),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+            color: Colors.white,
+          ),
+          child: Column(
+            children: [
+              const UserAccountsDrawerHeader(
+                accountName: Text("Trong Nguyen"),
+                accountEmail: Text("trong@gmail.com"),
+              ),
+              ListTile(
+                title: const Text("Help"),
+                leading: const Icon(Icons.question_mark),
+                onTap: () {
+                  // Handle the Search Contact action here
+                },
+              ),
+              ListTile(
+                title: const Text("Feedback"),
+                leading: const Icon(Icons.feedback_outlined),
+                onTap: () {
+                  // Handle the Search Contact action here
+                },
+              ),
+              ListTile(
+                title: const Text("System"),
+                leading: const Icon(Icons.settings),
+                onTap: () {
+                  // Handle the Add Contact action here
+                },
+              ),
+              ListTile(
+                title: const Text("Logout"),
+                leading: const Icon(Icons.logout_sharp),
+                onTap: () {
+                },
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
